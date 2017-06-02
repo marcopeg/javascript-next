2abd0fa1897a8675d036f4ae95cd56cab02e0a30
Marco Pegoraro <marco.pegoraro@gmail.com>
2017-06-02 13:31:16 +0200

    npm scripts

---
+++ 003-npm-scripts/README.md
@@ -0,0 +1,14 @@
+NPM Scripts
+================
+
+A task manager run repetitive tasks for you.
+
+## Alternatives
+
+### GruntJS
+
+https://gruntjs.com/
+
+### GulpJS
+
+http://gulpjs.com/
---
+++ 003-npm-scripts/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp')
+var clean = require('gulp-clean')
+var dependencies = require('gulp-html-dependencies')
+var webserver = require('gulp-webserver')
+var gulpSequence = require('gulp-sequence')
+
+gulp.task('clean:dist', function () {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+})
+
+gulp.task('copy:vendor', function () {
+    return gulp.src('src/**/*.html')
+        .pipe(dependencies({
+            dest: 'dist',
+            prefix: './vendor',
+        }))
+        .pipe(gulp.dest('dist'))
+})
+
+gulp.task('copy:assets', function () {
+    return gulp.src('src/assets/*')
+        .pipe(gulp.dest('dist/assets'))
+})
+
+gulp.task('build', gulpSequence('clean:dist', ['copy:vendor', 'copy:assets']))
+
+gulp.task('watch', function () {
+    gulp.watch('src/assets/**/*', ['copy:assets'])
+    gulp.watch('src/**/*.html', ['copy:vendor'])
+})
+
+gulp.task('serve', function () {
+    gulp.src('dist')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }))
+})
+
+gulp.task('default', gulpSequence(['build', 'serve', 'watch']))
